b0c5285c773ae6a64b1cd21479aef3b0798d597b
gerkai <gerkai202@gmail.com>
2023-04-06 12:13:54 -0700

    Initial Commit

---
+++ test3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  test3
+//
+//  Created by Geng Szoa on 4/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
