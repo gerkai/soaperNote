78fd65710749c97367bdd8f847153144f96d2b8d
gerkai <gerkai202@gmail.com>
2023-04-17 14:48:25 -0400

    final update ui

--- audioRecorder.swift
+++ audioRecorder.swift
@@ -1,10 +1,10 @@
-//
-//  audioRecorder.swift
-//  test3
-//
-//  Created by Geng Szoa on 4/11/23.
-//  Copyright © 2023 lingzhou125@gmail.com. All rights reserved.
-//
+    //
+    //  audioRecorder.swift
+    //  test3
+    //
+    //  Created by Geng Szoa on 4/11/23.
+    //  Copyright © 2023 lingzhou125@gmail.com. All rights reserved.
+    //
 
 import Foundation
 import AVFoundation
@@ -16,14 +16,24 @@ class AudioRecorder: NSObject, ObservableObject {
     @Published var isRecording = false
     var audioPlayer: AVAudioPlayer?
     @Published var isPlaying = false
-
+    
     private var silenceDetectionTimer: Timer?
     private let silenceThreshold: Float = -40.0 // Adjust this value to adjust the sensitivity of the silence detection
     private let silenceDetectionInterval: TimeInterval = 0.1 // Adjust this value to change the frequency of silence checks
     
     @Published var transcription = ""
     @Published var normalizedPower: CGFloat = 0.0
-
+    
+    func setupAudioSession() {
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(.playAndRecord, mode: .default, options: [.defaultToSpeaker, .allowBluetooth, .allowAirPlay])
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        } catch {
+            print("Failed to set up audio session: \(error)")
+        }
+    }
+    
     func startRecording() {
         
         setupAudioSession()
@@ -31,111 +41,111 @@ class AudioRecorder: NSObject, ObservableObject {
         let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
         let audioFilename = documentsPath.appendingPathComponent("recording.m4a")
         print("Recording file URL: \(audioFilename)")
-
+        
         let settings = [
             AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
             AVSampleRateKey: 12000,
             AVNumberOfChannelsKey: 1,
             AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
         ]
-
+        
         do {
-                    audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
-                    audioRecorder.delegate = self
-                    audioRecorder.isMeteringEnabled = true // Enable metering to detect silence
-                    audioRecorder.record()
-                    isRecording = true
-
-                    // Start silence detection timer
-                    silenceDetectionTimer = Timer.scheduledTimer(withTimeInterval: silenceDetectionInterval, repeats: true) { _ in
-                        self.checkForSilence()
-                    }
-                } catch {
-                    print("Could not start recording")
-                }
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.isMeteringEnabled = true // Enable metering to detect silence
+            audioRecorder.record()
+            isRecording = true
+            
+                // Start silence detection timer
+            silenceDetectionTimer = Timer.scheduledTimer(withTimeInterval: silenceDetectionInterval, repeats: true) { _ in
+                self.checkForSilence()
+            }
+        } catch {
+            print("Could not start recording")
+        }
     }
-
+    
     func stopRecording(shouldTranscribe: Bool = false) {
         audioRecorder.stop()
         isRecording = false
         
-        // Stop the silence detection timer
+            // Stop the silence detection timer
         silenceDetectionTimer?.invalidate()
         silenceDetectionTimer = nil
         
-    
-
+        
+        
             // Transcribe the last bit of audio
         if shouldTranscribe {
             let audioFileURL = audioRecorder.url
             print ("audiorecorder stopped")
             processAudioFile(audioFileURL, model: "whisper-1")
-            }
+        }
         
     }
     
     private func checkForSilence() {
         audioRecorder.updateMeters()
         let currentPower = audioRecorder.averagePower(forChannel: 0)
-
-        //for live meter
+        
+            //for live meter
         let normalizedPower = ((currentPower + 160) / 160)
         self.normalizedPower = CGFloat(min(max(normalizedPower, 0), 1))
         
         if currentPower < silenceThreshold {
-            // Detected silence
+                // Detected silence
             let audioFileURL = audioRecorder.url
             
-            // Calculate the recording duration
+                // Calculate the recording duration
             let duration = audioRecorder.currentTime
-
-            // Stop the current recording and start a new one
+            
+                // Stop the current recording and start a new one
             stopRecording(shouldTranscribe: false)
             
-
-            // Only send the audio file for transcription if the duration is longer than the threshold
+            
+                // Only send the audio file for transcription if the duration is longer than the threshold
             let durationThreshold: TimeInterval = 0.5 // Adjust the threshold value as needed
             if duration > durationThreshold {
-                // Send the recorded audio file to the Whisper API and update the transcription
-                // Replace "your-model-name" with the appropriate model name
+                    // Send the recorded audio file to the Whisper API and update the transcription
+                    // Replace "your-model-name" with the appropriate model name
                 processAudioFile(audioFileURL, model: "whisper-1")
                 
             }
             startRecording()
         }
     }
-
+    
     private func processAudioFile(_ fileURL: URL, model: String) {
         print("sending for transcription \(fileURL)")
         let apiKey = "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" // Replace this with your OpenAI API key
-
+        
         let url = URL(string: "https://api.openai.com/v1/audio/transcriptions")!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
-
+        
         let boundary = UUID().uuidString
         request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-
+        
         let body = NSMutableData()
-
+        
         let data = try! Data(contentsOf: fileURL)
         let filename = fileURL.lastPathComponent
-
+        
         body.append("--\(boundary)\r\n".data(using: .utf8)!)
         body.append("Content-Disposition: form-data; name=\"file\"; filename=\"\(filename)\"\r\n".data(using: .utf8)!)
         body.append("Content-Type: audio/wav\r\n\r\n".data(using: .utf8)!)
         body.append(data)
         body.append("\r\n".data(using: .utf8)!)
-
+        
         body.append("--\(boundary)\r\n".data(using: .utf8)!)
         body.append("Content-Disposition: form-data; name=\"model\"\r\n\r\n".data(using: .utf8)!)
         body.append("\(model)\r\n".data(using: .utf8)!)
-
+        
         body.append("--\(boundary)--\r\n".data(using: .utf8)!)
-
+        
         request.httpBody = body as Data
-
+        
         URLSession.shared.dataTask(with: request) { data, response, error in
             if let error = error {
                 print("Error: \(error.localizedDescription)")
@@ -144,27 +154,27 @@ class AudioRecorder: NSObject, ObservableObject {
                     if let decodedData = try? JSONDecoder().decode(TranscriptionResponse.self, from: data) {
                         DispatchQueue.main.async {
                             self.transcription += "\n" + decodedData.text // Append the new transcription text
-                            // callOpenAI(prompt: decodedData.text)
+                                                                          // callOpenAI(prompt: decodedData.text)
                             print (self.transcription)
                         }
                     } else {
                         print("Invalid response data")
-                        //self.status = "Sorry, an error occured"
+                            //self.status = "Sorry, an error occured"
                     }
                 } else {
                     print("Unexpected response status code: \(response.statusCode)")
-                    //self.status = "Sorry, an error occured"
+                        //self.status = "Sorry, an error occured"
                 }
             } else {
                 print("Unexpected error")
-                //self.status = "Sorry, an error occured"
+                    //self.status = "Sorry, an error occured"
             }
         }.resume()
     }
-
-        
-
-
+    
+    
+    
+    
     func playRecording() {
         let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
         let audioFilename = documentsPath.appendingPathComponent("recording.m4a")
@@ -178,10 +188,26 @@ class AudioRecorder: NSObject, ObservableObject {
             print("Could not play recording")
         }
     }
-
+    
     func stopPlaying() {
         audioPlayer?.stop()
         isPlaying = false
+    }
+    
+    func pausePlaying() {
+        print("---- paused")
+        audioPlayer?.pause()
+        isRecording = false
+    }
+    
+    func resumePlaying() {
+        print("---- resumed")
+        audioPlayer?.play()
+        isRecording = true
+    }
+    
+    func getCurrentTime() -> TimeInterval {
+        return audioPlayer?.duration ?? 1;
     }
 }
 
@@ -189,7 +215,7 @@ extension AudioRecorder: AVAudioRecorderDelegate {
     func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
         if !flag {
             print("Recording failed")
-         
+            
         }
     }
 }
